98615a2e6a11235c8680f54b41a27ce524933f78
Koliaha <haquanghuy@mail.ru>
2020-05-28 18:45:16 +0300

    commit popup policy second

--- js/script.js
+++ js/script.js
@@ -1,23 +1,37 @@
-(function($) {
-    $(function() {
-      $(".tablist").on("click", ".tabitem:not(.active)", function() {
-        $(this)
-          .addClass("active")
-          .siblings()
-          .removeClass("active")
-          .closest(".form-wrap")
-          .find(".tab-content")
-          .removeClass("active")
-          .eq($(this).index())
-          .addClass("active");
+(function ($) {
+  $(function () {
+    $(".tablist").on("click", ".tabitem:not(.active)", function () {
+      $(this)
+        .addClass("active")
+        .siblings()
+        .removeClass("active")
+        .closest(".form-wrap")
+        .find(".tab-content")
+        .removeClass("active")
+        .eq($(this).index())
+        .addClass("active");
+    });
+  });
+})(jQuery);
+
+$(document).ready( ()=> {
+  $(".regist-btn").click( ()=> {
+    $(".sec1").fadeOut(700,  ()=> {
+      $(".sec2").fadeIn(700);
+    });
+  });
+  $("a.popup-privacy").click( (event)=> {
+    event.preventDefault();
+    $(".darkscreen").fadeIn(700,  ()=> {
+      $(".close").click(  ()=> {
+        $(".darkscreen").fadeOut(700)
       });
     });
-  })(jQuery);
-
-  $(document).ready(function(){
-    $(".regist-btn").click(function(){
-      $(".sec1").fadeOut(700, function(){
-        $(".sec2").fadeIn(700);
-      } );
-      });
   });
+  $(".darkscreen").on('click', (e)=> {
+    var el = '.sec_policy';
+    if ($(e.target).closest(el).length) return;
+     {$(".darkscreen").fadeOut(700);}
+    
+   });
+});
